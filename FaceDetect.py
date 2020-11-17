ef09c68888a01312cfd0a3c2d3ad3b5f06a5a6dc
evanofslack <51209817+evanofslack@users.noreply.github.com>
2020-11-17 18:58:13 -0500

    Update FaceDetect.py
    Added mouth detecting cascade

--- FaceDetect.py
+++ FaceDetect.py
@@ -10,16 +10,19 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+#https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_smile.xml
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
+# Start video capture
 cap = cv2.VideoCapture(0)
 
-xvalues = []
 while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    print(faces[0])
+    # We want to pull out the first value in faces, but I'm not sure how to do it
+    # print(faces[0])
 
     if len(faces) == 0:
         print('-------------------------')
@@ -29,8 +32,6 @@ while 1:
         print("Face Detected")
 
     for (x,y,w,h) in faces:
-        
-        
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
@@ -48,12 +49,25 @@ while 1:
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
             
-    
             
-
-    #cv2.imshow('img',img)
+    
+        smile = smile_cascade.detectMultiScale(roi_gray)
+        
+        if len(smile) == 0:
+            print('-------------------------')
+            print("No Mouth Detected")
+        else:
+            print('-------------------------')
+            print("Mouth Detected")
+            
+        for (ex,ey,ew,eh) in smile:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+            
+            
+    
+    #cv2.imshow('img',img)   # This shows the video feed in real time, comment out when running headless
     k = cv2.waitKey(30) & 0xff
-    if k == 27:
+    if k == 27: # Break when 'q' is pressed
        break
 
 cap.release()
