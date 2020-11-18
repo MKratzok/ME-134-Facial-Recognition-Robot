7d62cba80796751d22c5ecf09f06be606fdb0e24
Max Kratzok <50670927+MKratzok@users.noreply.github.com>
2020-11-17 22:06:33 -0500

    Added buffer size and small change to order of print statements

--- FaceDetect.py
+++ FaceDetect.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from time import sleep
 
 # Good Theory
 # https://towardsdatascience.com/a-guide-to-face-detection-in-python-3eab0f6b9fc1
@@ -15,8 +16,12 @@ smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 # Start video capture
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CV_CAP_PROP_BUFFERSIZE, 2) # set buffer size to 2 to prevent lag
 
 while 1:
+    print('-------------------------')
+    print('Commencing Scan')
+
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
@@ -30,6 +35,7 @@ while 1:
     else:
         print('-------------------------')
         print("Face Detected")
+        # print(faces[0])
 
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -37,19 +43,17 @@ while 1:
         roi_color = img[y:y+h, x:x+w]
         
         eyes = eye_cascade.detectMultiScale(roi_gray)
-        # print(eyes)
-        
+
         if len(eyes) == 0:
             print('-------------------------')
             print("No Eyes Detected")
         else:
             print('-------------------------')
             print("Eyes Detected")
-        
+            # print(eyes)
+
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-            
-            
     
         smile = smile_cascade.detectMultiScale(roi_gray)
         
@@ -63,8 +67,7 @@ while 1:
         for (ex,ey,ew,eh) in smile:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
             
-            
-    
+
     #cv2.imshow('img',img)   # This shows the video feed in real time, comment out when running headless
     k = cv2.waitKey(30) & 0xff
     if k == 27: # Break when 'q' is pressed
